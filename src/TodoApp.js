5ad296fc4087e8481923b66a2975a90d543084db
edgar humberto perez martinez <edgar@localhost.localdomain>
2020-04-27 17:21:31 -0500

    todo app with hooks

---
+++ src/TodoApp.js
@@ -0,0 +1,55 @@
+import React, {useEffect} from 'react';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
+import useTodoState from './hooks/useTodoState';
+
+function TodoApp() {
+    const initialTodos = JSON.parse(window.localStorage.getItem('todos') || '[]');
+    const {todos,addTodo,removeTodo,toggleTodo,editTodo} = useTodoState(initialTodos);
+    // const initialTodos = [
+    //     {id: 1, task: 'clean fishtank', completed: false},
+    //     {id: 2, task: 'wash car', completed: true },
+    //     {id: 3, task: 'grow beard', completed: false}
+    // ];
+
+    useEffect(() => {
+        window.localStorage.setItem("todos",JSON.stringify(todos));
+    },[todos]);
+
+
+    return (
+        <Paper
+         style={{
+             padding: '0',
+             margin: '0',
+             height: '100vh',
+             backgroundColor: 'grey'
+         }}
+         elevation={0}
+        >
+            <AppBar color='primary' position='static' style={{height: '64px'}}>
+                <Toolbar>
+                    <Typography>TODOS WITH HOOKS</Typography>
+                </Toolbar>
+            </AppBar>
+            <Grid container justify='center' style={{marginTop: '1rem'}} >
+                <Grid item xs={11} md={8} lg={4}>
+                    <TodoForm addTodo={addTodo} />
+                    <TodoList 
+                     todos={todos} 
+                     removeTodo={removeTodo} 
+                     toggleTodo={toggleTodo}
+                     editTodo={editTodo}
+                     />
+                </Grid>
+            </Grid>
+        </Paper>
+    );
+}
+
+export default TodoApp;
---
+++ src/TodoForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import useInputState from './hooks/useInputState';
+
+function TodoForm({addTodo}) {
+    const [value,handleChange,reset] = useInputState("");
+    return (
+        <Paper style={{margin: '1rem 0', padding: '0 1rem'}}>
+            <form onSubmit={e => {
+                e.preventDefault();
+                addTodo(value);
+                reset();}}
+                style={{marginLeft: '2rem',width:'50%'}}
+                >
+                <TextField 
+                 value={value} 
+                 onChange={handleChange} 
+                 margin='normal'
+                 label='Add new Todo'
+                 fullWidth
+                 autoFocus
+                 />
+            </form>
+        </Paper>
+    );
+}
+
+export default TodoForm;
